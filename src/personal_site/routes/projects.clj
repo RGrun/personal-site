655d24e3eb4aa5d18be5f9bee8751f79c276bf3c
RGrun <richard@r3software.org>
2016-04-11 17:58:56 -0700

    added adc page

--- src/personal_site/routes/projects.clj
+++ src/personal_site/routes/projects.clj
@@ -3,18 +3,21 @@
             [personal-site.views.layout :as layout]))
 
 (defn malbum-page []
-  (layout/render "malbum.html" {:title "Malbum <~> Projects <~> Richard Grunert"}))
+      (layout/render "malbum.html" {:title "Malbum <~> Projects <~> Richard Grunert"}))
 
 (defn lunar-invasion-page []
-  (layout/render "invasion.html" {:title "Lunar Invasion <~> Projects <~> Richard Grunert"}))
+      (layout/render "invasion.html" {:title "Lunar Invasion <~> Projects <~> Richard Grunert"}))
+
+(defn adc-page []
+      (layout/render "adc.html" {:title "Accident Data Center <~> Projects <~> Richard Grunert"}))
 
 (defn athena-page []
-  (layout/render "athena.html" {:title "Athena <~> Projects <~> Richard Grunert"}))
+      (layout/render "athena.html" {:title "Athena <~> Projects <~> Richard Grunert"}))
 
 (defroutes project-routes
     (GET "/projects/lunar-invasion" [] (lunar-invasion-page))
 ;    (GET "/projects/malbum" [] (malbum-page))
-;    (GET "/projects/adc" [] (adc-page))
+    (GET "/projects/adc" [] (adc-page))
 ;    (GET "/projects/other-sites" (other-sites-page))
     (GET "/projects/athena" [] (athena-page))
     )
