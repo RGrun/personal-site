3c51cb162de28aa05431da017d02b2e6b159867e
RGrun <richard@r3software.org>
2016-04-11 18:20:20 -0700

    added other sites page

--- src/personal_site/routes/projects.clj
+++ src/personal_site/routes/projects.clj
@@ -11,6 +11,9 @@
 (defn adc-page []
       (layout/render "adc.html" {:title "Accident Data Center <~> Projects <~> Richard Grunert"}))
 
+(defn other-sites-page []
+      (layout/render "other-sites.html" {:title "Other Sites <~> Projects <~> Richard Grunert"}))
+
 (defn athena-page []
       (layout/render "athena.html" {:title "Athena <~> Projects <~> Richard Grunert"}))
 
@@ -18,6 +21,6 @@
     (GET "/projects/lunar-invasion" [] (lunar-invasion-page))
 ;    (GET "/projects/malbum" [] (malbum-page))
     (GET "/projects/adc" [] (adc-page))
-;    (GET "/projects/other-sites" (other-sites-page))
+    (GET "/projects/other-sites" [] (other-sites-page))
     (GET "/projects/athena" [] (athena-page))
     )
